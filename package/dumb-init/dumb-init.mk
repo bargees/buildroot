6a34c4918a5184f3d9b954c95bd6198421f1222a
A.I <ailis@paw.zone>
2016-10-05 17:00:48 -0700

    dumb-init: bump to version 1.2.0

--- package/dumb-init/dumb-init.mk
+++ package/dumb-init/dumb-init.mk
@@ -4,15 +4,15 @@
 #
 ################################################################################
 
-DUMB_INIT_VERSION = v1.0.2
-DUMB_INIT_SITE = https://github.com/Yelp/dumb-init/archive
-DUMB_INIT_SOURCE = $(DUMB_INIT_VERSION).tar.gz
-
+DUMB_INIT_VERSION = v1.2.0
+DUMB_INIT_SITE = $(call github,Yelp,dumb-init,$(DUMB_INIT_VERSION))
 DUMB_INIT_LICENSE = MIT
 DUMB_INIT_LICENSE_FILES = LICENSE
 
+DUMB_INIT_MAKE_OPTS = CC="$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS)"
+
 define DUMB_INIT_BUILD_CMDS
-	$(TARGET_CC) -std=gnu99 -static -s -Wall -O3 -o $(@D)/dumb-init $(@D)/dumb-init.c
+	$(TARGET_MAKE_ENV) $(MAKE) $(DUMB_INIT_MAKE_OPTS) -C $(@D)
 endef
 
 define DUMB_INIT_INSTALL_TARGET_CMDS
