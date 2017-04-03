17fa4c77bccfc25b78f9ac5e06f2bcfc4edc0e36
A.I <ailis@paw.zone>
2017-04-03 08:34:58 -0700

    rpi-firmware: upgrade for kernel 4.9.20

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 7f8ac8dac0b80291cbf5e56580139034a0a42070
+RPI_FIRMWARE_VERSION = 8c84479cfe05d60ef78730924e85c121b1efd0c7
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
