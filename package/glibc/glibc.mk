f4be5deae9b7dc4890890a843d775ef2daea4fdc
A.I <ailis@paw.zone>
2017-12-07 23:11:28 -0800

    package/glibc: update with upstream patches
    https://github.com/bminor/glibc/tree/release/2.25/master

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -11,7 +11,7 @@ GLIBC_SOURCE = glibc-$(GLIBC_VERSION).tar.gz
 else
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/2.25/master
-GLIBC_VERSION = b0afcf5db758747c2e563a1db0af6db7a1927746
+GLIBC_VERSION = 4444f6a92b83f7e044705b43b11dcdb0dbe97fe1
 # Upstream doesn't officially provide an https download link.
 # There is one (https://sourceware.org/git/glibc.git) but it's not reliable,
 # sometimes the connection times out. So use an unofficial github mirror.
